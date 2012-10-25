44df266e7ae47095b66687bb97e8a1039e1ceecc
Kris Gonzalez <gonzo@thoughtworks.com>
2012-10-25 00:19:16 -0700

    removing duplicate CGI escaped strings, fixing broken test

--- lib/yelp_service.rb
+++ lib/yelp_service.rb
@@ -5,8 +5,8 @@ class YelpService
 
   def self.find_restaurants(cuisine, location, limit=2)
     query_params = {ywsid: Sfrubytalkv2::Application.config.yelp_v1_key,
-                    term: CGI.escape(cuisine),
-                    location: CGI.escape(location)}
+                    term: cuisine,
+                    location: location}
     raw_response = send_get(API_HOST, '/business_review_search', query_params)
     results = []
     raw_response.businesses.each do |restaurant_info|
