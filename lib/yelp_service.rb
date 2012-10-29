003dd42f20d45ad85ec6b27383fecd42cdd8734c
Kris Gonzalez <gonzo@thoughtworks.com>
2012-10-28 23:35:24 -0700

    refactored the yelp service to be consistent with the url shortening service

--- lib/yelp_service.rb
+++ lib/yelp_service.rb
@@ -1,18 +1,21 @@
-class YelpService
+module YelpService
   extend HttpAbstraction
 
-  API_HOST = 'http://api.yelp.com'
-
-  def self.find_restaurants(cuisine, location, limit=2)
-    query_params = {ywsid: Sfrubytalkv2::Application.config.yelp_v1_key,
-                    term: cuisine,
-                    location: location}
-    raw_response = send_get(API_HOST, '/business_review_search', query_params)
-    results = []
-    raw_response.businesses.each do |restaurant_info|
-      results << YelpRestaurantResult.from_service_response(restaurant_info)
-      break if results.size == limit
+  class << self
+    def find_restaurants(cuisine, location, limit=2)
+      raw_response = send_get('http://api.yelp.com', '/business_review_search', params(cuisine, location))
+      results = []
+      raw_response.businesses.each do |restaurant_info|
+        results << YelpRestaurantResult.from_service_response(restaurant_info)
+        break if results.size == limit
+      end
+      results
     end
-    results
+
+    def params(cuisine, location)
+      {ywsid: Sfrubytalkv2::Application.config.yelp_v1_key,
+       term: cuisine,
+       location: location}
+    end
   end
 end
